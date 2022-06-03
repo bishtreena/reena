c9e6dab718068e01db63641acbcf8f134d771059
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-03 23:54:48 +0530

    Create 23.py

---
+++ 23.py
@@ -0,0 +1,6 @@
+#check number divisible by 5 and 11
+a=int(input("enter the number:"))
+if a%5==0 and a%11==0:
+    print("number is divisible by 5 and 11")
+else:
+    print("number not divisible by 5 and 11")
