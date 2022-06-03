455cadb9b7c9959e379e91f77c57a7ea14e2414f
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-03 23:21:25 +0530

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,7 @@
+#largest among two number
+a=int(input ("enter 1 number="))
+b=int (input("enter 2 number="))
+if a>b:
+    print('{0} is largest'.format(a))
+else:
+    print('{0} is largest'.format(b))
