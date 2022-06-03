2c3e78c26c0821d3f6603412ac046a4340ad19ec
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-03 23:58:02 +0530

    Create 28.py

---
+++ 28.py
@@ -0,0 +1,6 @@
+#find simple interest
+p=int(input("enter principal amount:"))
+t=int(input("enter time in year:"))
+r=int(input("enter  rate of interest:"))
+si=(p*t*r)/100
+print(" simple interest ={0}".format(si))
