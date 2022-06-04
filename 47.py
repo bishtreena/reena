865f1bebdc49041e12e83ef3ec6c4efa09417c95
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-04 21:44:25 +0530

    Create 47.py

---
+++ 47.py
@@ -0,0 +1,10 @@
+#find sum and average of natural numbers
+sum=0
+n=int (input("enter number:"))
+for i in range(1,n):
+    print(i)
+    sum=sum+i
+print("sum of number:{0}".format(sum))
+avg=sum/(n-1)
+print("average of number :{0}".format(avg))
+
