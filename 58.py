4ac903508a8ed28c29757f754245cc0d738c7f7d
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-05 18:50:59 +0530

    Create 58.py

---
+++ 58.py
@@ -0,0 +1,6 @@
+#to find GCD of two numbers
+import math
+x=int(input("enter first number:"))
+y=int(input("enter second number:"))
+print("print the gcd of {0} and {1} is:".format(x,y))
+print(math.gcd(x,y))
