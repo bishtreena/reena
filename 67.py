79ebbd7be325805458cc665a11ec3ce17818315d
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-20 16:49:54 +0530

    Update 67.py

--- 67.py
+++ 67.py
@@ -1,10 +1,15 @@
-#print harshad number from 1 to 100
-for num in range(1,101):
-  num1=num
-  sum=0
-  while(num1!=0):
-    temp=num1%10
-    sum=sum+temp
-    num1=num1//10
-  if(num%sum==0):
-    print(num)
+#check krishnamurthy number
+num=int(input("Enter number:"))
+num1=num
+sum=0
+while(num1!=0):
+  fact = 1
+  temp=num1%10
+  for i in range(1,temp+1):
+    fact=fact*i
+  sum=sum+fact
+  num1=num1//10
+if(num==sum):
+    print("{0} is a krisnamurthy number:".format(sum))
+else:
+    print("{0} is not a krisnamurthy number:".format(sum))
