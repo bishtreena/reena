3a3e89bb9943ad1425d03be0b2707ba8251e5e4b
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-20 16:59:46 +0530

    Create 79.py

---
+++ 79.py
@@ -0,0 +1,16 @@
+#check strong number
+num=int(input("Enter number:"))
+num1=num
+sum=0
+while(num1!=0):
+  fact = 1
+  temp=num1%10
+  for i in range(1,temp+1):
+    fact=fact*i
+  sum=sum+fact
+  num1=num1//10
+if(num==sum):
+    print("{0} is a strong number:".format(sum))
+else:
+    print("{0} is not a strong number:".format(sum))
+
