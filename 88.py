70290186369dfabfbca93ca480adb5766b11577b
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-20 17:08:49 +0530

    Create 88.py

---
+++ 88.py
@@ -0,0 +1,6 @@
+#check vowel or consonant
+ch=input("Enter the character:")
+if((ch=='A'or ch=='E'or ch=='I'or ch=='O'or ch=='U')or(ch=='a'or ch=='e' or ch=='i' or ch=='o'or ch=='u')):
+    print(ch,"is vowel")
+else:
+    print(ch,"is a consonant")    
