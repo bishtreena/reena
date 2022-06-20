04d712723951394046ca83cfed2442392aaf0111
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-20 16:59:08 +0530

    Create 78.py

---
+++ 78.py
@@ -0,0 +1,8 @@
+#program to reverse a number
+n=int(input("Enter range:"))
+for num in range(1,n+1):
+    print(num)
+print("number in reverse order:")
+for j in range(num,0,-1):
+    print(j)
+        
