231af8343688456593c1fb6618d628636105ba5d
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-20 17:06:30 +0530

    Create 84.py

---
+++ 84.py
@@ -0,0 +1,6 @@
+#check alphabet or digit
+ch=input("Enter any chracter:")
+if(ch.isdigit()):
+    print("the given character",ch,"is a digit")
+else:
+    print('the given character',ch,'is a alphabet')      
