e3ef626edc282482a9be3f2c3309be5ccca9a125
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-20 17:04:50 +0530

    Create 82.py

---
+++ 82.py
@@ -0,0 +1,8 @@
+#swap two number
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+temp=n2
+n2=n1
+n1=temp
+print("number aftr swapping:")
+print("n1={0},n2={1}".format(n1,n2))
