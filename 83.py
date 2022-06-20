e9dfea51eb8e2a06497c819b35f0632a607dea23
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-20 17:05:51 +0530

    Create 83.py

---
+++ 83.py
@@ -0,0 +1,6 @@
+#to check alphabet or not
+ch=input("Enter the character:")
+if((ch>='a'and ch<='z')or(ch>='A'and ch<='B')):
+    print(ch,"is an alphabet")
+else:
+    print(ch,"is not an alphabet")
