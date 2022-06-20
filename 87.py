347321c57b3715aad2a5d858708bee31fb20e35c
bishtreena <106833282+bishtreena@users.noreply.github.com>
2022-06-20 17:08:13 +0530

    Create 87.py

---
+++ 87.py
@@ -0,0 +1,6 @@
+#check uppercase or not
+ch=input("Enter the character:")
+if(ch.isupper()):
+    print("given character",ch,"is uppercase")
+else:
+    print("given character",ch,"is not uppercase")    
